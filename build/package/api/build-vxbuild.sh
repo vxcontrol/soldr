a244ee7e404d2a24c9a172079991d94cebd96dc9
Roman <romq1@mail.ru>
2022-11-28 14:46:21 +0300

    use latest vxcontrol/vxbuild-cross for building api and server

--- build/package/api/build-vxbuild.sh
+++ build/package/api/build-vxbuild.sh
@@ -8,4 +8,4 @@ docker run --rm -it \
         -v $(realpath "$SCRIPT_DIR/../../../../"):/go/src/ \
         -v $(go env GOMODCACHE):/go/pkg/mod \
         --workdir=/go/src/soldr \
-        vxcontrol/vxbuild-cross:1.19.1 /bin/bash -c "build/package/server/build-local.sh"
+        vxcontrol/vxbuild-cross:latest /bin/bash -c "build/package/server/build-local.sh"
