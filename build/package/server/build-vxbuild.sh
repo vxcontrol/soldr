ca54d8a613a656660f2b3f0e6ec3e69d9c1b9972
Aleksey Nikitin <imsouz9@gmail.com>
2022-11-29 12:27:14 +0300

    Run building in docker as local user

--- build/package/server/build-vxbuild.sh
+++ build/package/server/build-vxbuild.sh
@@ -5,7 +5,9 @@ set -e
 SCRIPT_DIR="$(dirname "$0")"
 
 docker run --rm -it \
+        -u $(id -u):$(id -g) \
         -v $(realpath "$SCRIPT_DIR/../../../../"):/go/src/ \
         -v $(go env GOMODCACHE):/go/pkg/mod \
+        -e GOCACHE=/tmp \
         --workdir=/go/src/soldr \
         vxcontrol/vxbuild-cross:latest /bin/bash -c "build/package/server/build-$(go env GOOS)-$(go env GOARCH).sh"
