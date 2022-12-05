0a6a1a1710878a2f78eddb828c814edd888c28f3
Aleksey Nikitin <imsouz9@gmail.com>
2022-12-05 17:50:33 +0300

    Fix typo: build server on `api` target

--- build/package/api/build-vxbuild.sh
+++ build/package/api/build-vxbuild.sh
@@ -10,4 +10,4 @@ docker run --rm -it \
         -v $(go env GOMODCACHE):/go/pkg/mod \
         -e GOCACHE=/tmp \
         --workdir=/go/src/soldr \
-        vxcontrol/vxbuild-cross:latest /bin/bash -c "build/package/server/build-local.sh"
+        vxcontrol/vxbuild-cross:latest /bin/bash -c "build/package/api/build-local.sh"
