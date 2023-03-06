979dd16c62517e64baf7ea0754c791ace89bbfdb
Dmitriy Merk <mervid@mail.ru>
2023-03-06 11:04:19 +0300

    fix(ui): enable optimize minification (#95)
    * fix: enable optimization minimizer
    
    * fix: fix comments

--- web/extra-webpack.config.js
+++ web/extra-webpack.config.js
@@ -1,4 +1,5 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = (config) => {
     config.module.rules.unshift(
@@ -13,6 +14,16 @@ module.exports = (config) => {
     );
 
     config.plugins.push(new MonacoWebpackPlugin());
+
+    config.optimization.minimize = true;
+
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        minify: TerserPlugin.terserMinify,
+        test: /\.js$/i
+      }),
+      ...(config.optimization.minimizer || [])
+    ];
 
     return config;
 };
